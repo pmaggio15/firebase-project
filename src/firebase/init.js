34dab4d76e983ff00a96c9941f19a2b36d819975
Your Name <you@example.com>
2025-08-13 09:50:09 -0600

    first commit

---
+++ src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-VbrCGNubr7h4xyAlYUNmT-OB6-vOEVs",
+  authDomain: "fir-project-583b7.firebaseapp.com",
+  projectId: "fir-project-583b7",
+  storageBucket: "fir-project-583b7.firebasestorage.app",
+  messagingSenderId: "502798190057",
+  appId: "1:502798190057:web:0ae8645720573549a17778"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
