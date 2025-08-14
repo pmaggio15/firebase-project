d469cf9ffea194f57dcb445b10a1bd93d53fd885
Your Name <you@example.com>
2025-08-13 20:09:04 -0600

    crud create read update delete cheat sheet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { auth } from './firebase/init';
+import { auth, db } from './firebase/init';
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc} from 'firebase/firestore';
 import { 
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
@@ -12,10 +13,59 @@ function App() {
   const [user, setUser] = React.useState({});
   const [loading, setLoading] = React.useState(true);
 
+  async function updatePost() {
+    const hardcodedId = "1f2322C6EibXPQxxyvy7";
+    const postRef = doc(db, "posts", hardcodedId);
+    const post = await getPostById(hardcodedId)
+    console.log(post)
+    const newPost = {
+      ...post,
+      title: "Land a $400k job",
+    };
+    console.log(newPost);
+    updateDoc(postRef, newPost);
+  }
+
+  function deletePost() {
+    const hardcodedId = "1f2322C6EibXPQxxyvy7";
+    const postRef = doc(db, "posts", hardcodedId);
+    deleteDoc(postRef);
+  }
+
+  function createPost() {
+    const post = {
+      title: "Finish Interview Section",
+      description: "Do Frontend Simplified",
+      uid: user.uid,
+    };
+    addDoc(collection(db, "posts"), post)
+  }
+
+  async function getAllPosts () {
+     const { docs } = await getDocs(collection(db, "posts"));
+     const posts = docs.map((elem) => ({...elem.data(), id: elem.id }));
+     console.log(posts);
+  }
+
+  async function getPostById(id) {
+    const postRef = doc(db, "posts", id);
+    const postSnap = await getDoc(postRef);
+    return postSnap.data();
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, "posts"),
+      where("uid", "==", user.uid)
+    );
+    const { docs } = await getDocs(postCollectionRef);
+    console.log(docs.map(doc => doc.data()));
+  }
+
+
   React.useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       setLoading(false);
-      console.log(user.email[0].toUpperCase);
       if(user) {
         setUser(user)
       }
@@ -55,7 +105,13 @@ function App() {
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
       <button onClick={logout}>Logout</button>
-      {loading? 'loading...' : user.email}
+      {loading ? 'loading...' : user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
